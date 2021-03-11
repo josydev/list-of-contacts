906f0c81ea410f81f717c3b7ca58746929a99ffd
Josyane T <32505778+josyscript@users.noreply.github.com>
2021-03-11 19:55:38 -0300

    Remove unused files unused

--- components/Input.vue
+++
@@ -1,6 +0,0 @@
-<select name="select" @click="select = $event.target.value">
-        <option value="">filter</option>
-        <option v-for="user of list" :key="user.id" :value="user.company.bs">
-          {{ user.company.bs }}
-        </option>
-      </select>
